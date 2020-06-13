566282792a588151f8b66172bdcfd411271ecf0c
roanmertens <roanmertens91@gmail.com>
2020-06-13 14:13:36 +0100

    add initial docker setup to project

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2020_05_03_121430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "members", force: :cascade do |t|
-    t.string "name"
-    t.text "about"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
